f3380aa1761f91286e019ff681b2f5469aae7896
Abu <mdabucse@gmail.com>
2026-04-30 16:38:00 +0530

    [ADD] Day-5 C# Basics

---
+++ Day-5/basics_of_c_sharp/utils/jump_game_ii.cs
@@ -0,0 +1,20 @@
+public class JumpGameClass {
+    public int JumpGame(int[] nums) {
+        
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < nums.Length - 1; i++) {
+            
+            farthest = Math.Max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+            }
+        }
+
+        return jumps;
+    }
+}
---
+++ Day-5/basics_of_c_sharp/utils/longest_palindrome_substring.cs
@@ -0,0 +1,36 @@
+
+public class LongestPalindromeClass{
+    public string LongestPalindrome(string s) {
+        
+        string res = "";
+        int resLen = 0;
+
+        for (int i = 0; i < s.Length; i++) {
+
+            // Odd length palindrome
+            int l = i, r = i;
+            while (l >= 0 && r < s.Length && s[l] == s[r]) {
+                if (resLen < (r - l + 1)) {
+                    res = s.Substring(l, r - l + 1);
+                    resLen = r - l + 1;
+                }
+                l--;
+                r++;
+            }
+
+            // Even length palindrome
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < s.Length && s[l] == s[r]) {
+                if (resLen < (r - l + 1)) {
+                    res = s.Substring(l, r - l + 1);
+                    resLen = r - l + 1;
+                }
+                l--;
+                r++;
+            }
+        }
+
+        return res;
+    }
+}
